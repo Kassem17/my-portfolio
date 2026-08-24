75820417f64896d25853fde589a50ce247aba80f
Kassem Haidar <kassemhaidar290@gmail.com>
2026-08-25 01:03:30 +0300

    fixing vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,39 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
   server: {
     port: 3000,
   },
+
   build: {
     target: "esnext",
     cssCodeSplit: true,
     chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("three") || id.includes("@react-three")) {
-              return "vendor-three";
-            }
-            if (id.includes("framer-motion")) {
-              return "vendor-framer-motion";
-            }
-            if (id.includes("recharts")) {
-              return "vendor-recharts";
-            }
-            if (id.includes("react") || id.includes("react-dom") || id.includes("react-router")) {
-              return "vendor-react";
-            }
-            if (id.includes("@supabase")) {
-              return "vendor-supabase";
-            }
-            return "vendor-utils";
-          }
-        },
-      },
-    },
   },
 });
